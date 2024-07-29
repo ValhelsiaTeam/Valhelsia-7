f0e03ac8f2dcbeb02321aa0faec96479ac9999df
Khytwel <54046029+Khytwel@users.noreply.github.com>
2024-07-29 22:18:39 +0200

    Create block_tags.js

---
+++ kubejs/server_scripts/tags/block_tags.js
@@ -0,0 +1,44 @@
+// priority: 110
+//   __   ___   _    _  _ ___ _    ___ ___   _      _____
+//   \ \ / /_\ | |  | || | __| |  / __|_ _| /_\    /__  /
+//    \ V / _ \| |__| __ | _|| |__\__ \| | / _ \     / /
+//     \_/_/ \_\____|_||_|___|____|___/___/_/ \_\   /_/
+//                       
+
+/**
+ * @file Block Tag Additions for Valhelsia 7.
+ * 
+ * These tags differ from item tags in that they impact the block form only,
+ * and therefore mainly have in-world effects. 
+ * 
+ * @see block_tags_remove.js
+ * 
+ * @copyright Valhelsia Inc 2024
+ */
+
+/**
+ * Block Tag Event Handler
+ */
+ServerEvents.tags('block', event => {
+  // Create
+  //event.add('create:fan_processing_catalysts/haunting', [
+  //  'valhelsia_structures:soul_brazier',
+  //]);
+
+  //event.add('create:fan_processing_catalysts/smoking', [
+  //  'valhelsia_structures:brazier',
+  //]);
+
+  // Mekanism
+  event.add('mekanism:cardboard_blacklist', [
+    'waystones:waystone',
+    'waystones:mossy_waystone',
+    'waystones:sandy_waystone'
+  ]);
+
+  // Missing Quark Revertable Chests
+  //event.add('quark:revertable_chests', [
+  //  'ars_nouveau:archwood_chest'
+  //]);
+  
+});
