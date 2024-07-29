6e9941dc3aa946284119df54b40a222b2d1916a8
Khytwel <54046029+Khytwel@users.noreply.github.com>
2024-07-29 22:18:41 +0200

    Create block_tags_remove.js

---
+++ kubejs/server_scripts/tags/block_tags_remove.js
@@ -0,0 +1,19 @@
+// priority: 120
+//   __   ___   _    _  _ ___ _    ___ ___   _      _____
+//   \ \ / /_\ | |  | || | __| |  / __|_ _| /_\    /__  /
+//    \ V / _ \| |__| __ | _|| |__\__ \| | / _ \     / /
+//     \_/_/ \_\____|_||_|___|____|___/___/_/ \_\   /_/
+//                       
+
+/**
+ * @file Block Tag Removal for Valhelsia 7.
+ * @copyright Valhelsia Inc 2024
+ */
+
+/**
+ * Block Tag Event Handler (to be used for tag removal only).
+ * @see block_tags.js
+ */
+ServerEvents.tags('block', event => {
+  
+});
