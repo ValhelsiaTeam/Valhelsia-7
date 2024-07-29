2a9893499c4a4005aca3ad693a96acf28abbd031
Khytwel <54046029+Khytwel@users.noreply.github.com>
2024-07-29 22:18:50 +0200

    Create item_tags_remove.js

---
+++ kubejs/server_scripts/tags/item_tags_remove.js
@@ -0,0 +1,19 @@
+// priority: 120
+//   __   ___   _    _  _ ___ _    ___ ___   _      _____
+//   \ \ / /_\ | |  | || | __| |  / __|_ _| /_\    /__  /
+//    \ V / _ \| |__| __ | _|| |__\__ \| | / _ \     / /
+//     \_/_/ \_\____|_||_|___|____|___/___/_/ \_\   /_/
+//                       
+
+/**
+ * @file Item Tag Removal for Valhelsia 7.
+ * @copyright Valhelsia Inc 2024
+ */
+
+/**
+ * Item Tag Event Handler (to be used for tag removal only).
+ * @see item_tags.js
+ */
+ServerEvents.tags('item', event => {
+
+});
