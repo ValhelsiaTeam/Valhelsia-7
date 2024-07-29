ed1a7111463dc20838bcabcb774fd48d395fb657
Khytwel <54046029+Khytwel@users.noreply.github.com>
2024-07-29 22:18:46 +0200

    Create fluid_tags.js

---
+++ kubejs/server_scripts/tags/fluid_tags.js
@@ -0,0 +1,31 @@
+// priority: 120
+//   __   ___   _    _  _ ___ _    ___ ___   _      _____
+//   \ \ / /_\ | |  | || | __| |  / __|_ _| /_\    /__  /
+//    \ V / _ \| |__| __ | _|| |__\__ \| | / _ \     / /
+//     \_/_/ \_\____|_||_|___|____|___/___/_/ \_\   /_/
+//                       
+
+/**
+ * @file Fluid Tag Changes for Valhelsia 7.
+ * @copyright Valhelsia Inc 2024
+ */
+
+/**
+ * Fluid Tag Event Handler.
+ */
+ServerEvents.tags('fluid', event => {
+  //vent.remove('minecraft:water', [
+  //  'ad_astra:fuel',
+  //  'ad_astra:flowing_fuel',
+  //  'ad_astra:oil',
+  //  'ad_astra:flowing_oil',
+  //  'ad_astra:cryo_fuel',
+  //  'ad_astra:flowing_cryo_fuel',
+  //  'createaddition:bioethanol',
+  //  'createaddition:flowing_bioethanol',
+  //  'createaddition:seed_oil',
+  //  'createaddition:flowing_seed_oil',
+  //  'enlightened_end:ooze_fluid',
+  //  'enlightened_end:flowing_ooze_fluid',
+  //]);
+});
