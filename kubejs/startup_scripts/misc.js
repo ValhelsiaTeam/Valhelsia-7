50d1b247c34e30f59ca1b4b1b40fac367a49fd21
Vaelzan <vaelzan@evilcult.net>
2024-08-05 20:53:17 +1000

    Set display name for valhelsia and kubejs namespace.

---
+++ kubejs/startup_scripts/misc.js
@@ -0,0 +1,14 @@
+// priority: 90
+//   __   ___   _    _  _ ___ _    ___ ___   _      _____
+//   \ \ / /_\ | |  | || | __| |  / __|_ _| /_\    /__  /
+//    \ V / _ \| |__| __ | _|| |__\__ \| | / _ \     / /
+//     \_/_/ \_\____|_||_|___|____|___/___/_/ \_\   /_/
+//
+
+/**
+ * @file Misc startup script contents for Valhelsia 7.
+ */
+
+Platform.getInfo('valhelsia').name = 'Valhelsia';
+Platform.mods.valhelsia.name = 'Valhelsia';
+Platform.mods.kubejs.name = 'Valhelsia';
